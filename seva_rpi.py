e59adf1947ce9c457b25f164ef4537cc94155269
selvincephus <selvincephus@gmail.com>
2018-09-06 17:29:54 +0000

    Creating rpi testing branch.

--- seva_rpi.py
+++ seva_rpi.py
@@ -7,7 +7,7 @@ import wave
 
 WAVE_FILE_NAME = "test1.wav"
 
-subprocess.call(["sox", "-t", "waveaudio", "-c", "1", "-r", "16000", "-d", WAVE_FILE_NAME, "silence", "1", "0.1", "3%", "1", "3.0", "3%"])
+subprocess.call(["rec", "-c", "1", "-r", "16000", WAVE_FILE_NAME, "silence", "1", "0.1", "3%", "1", "3.0", "3%"])
 print("Recording done..")
 subprocess.call(["aplay", WAVE_FILE_NAME])
 
